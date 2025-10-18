8bd64f520e350e50e39a67b07fcb5df8978ef920
maximomireles7 <maximon380@gmail.com>
2025-10-18 16:08:37 -0600

    Estructura del proyecto

---
+++ app/src/main/java/com/example/musicmax/ui/detail/DetailScreen.kt
@@ -0,0 +1,2 @@
+package com.example.musicmax.ui.detail
+
---
+++ app/src/main/java/com/example/musicmax/ui/theme/Theme.kt
@@ -0,0 +1,18 @@
+package com.example.musicmax.ui.theme
+
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+fun MusicMaxTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+
+}
